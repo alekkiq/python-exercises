439dead0bf13b56c101581101bbb407c0afdb556
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-08-23 14:17:58 +0300

    Setup module 8

--- Modules/mod08.py
+++ Modules/mod08.py
@@ -1,26 +1,15 @@
 import mysql.connector
-from db_helpers.airport_db_populator import *
+
+from db_helpers.populate_airports_db import populate_database
+from db_helpers.csv_helpers import get_file_path
 
 # first put some data to the db
-fill_database(read_json("airports.json"), "airports_db")
+# building the db helpers was most likely a larger job than the exercise itself :D
+populate_database(get_file_path("airports.csv"), "airports")
 
 # 1
 def get_airport_by_icao():
-    icao = input("Input a valid Airport ICAO-code: ")
-
-    db = db_connection()
-    cursor = db.cursor(dictionary=True)
-
-    cursor.execute("USE airports_db;")
-    
-    query = f"SELECT name, municipality FROM airports WHERE icao = '{icao.upper()}';"
-
-    cursor.execute(query)
-
-    for item in cursor.fetchall():
-        print(f"\nAirport {icao}:\n")
-        print(f"Name: {item["name"]}")
-        print(f"Municipality: {item["municipality"]}\n")
+    print("")
 
 
 # test with these ICAOs: KLAX, RJTT, EGLL
