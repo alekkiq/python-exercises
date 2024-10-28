2c4cbcbaba921fdabec3b68e8d975916812f8584
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-10-28 10:24:24 +0200

    update mod13

--- Modules/mod13/1.py
+++ Modules/mod13/1.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
@@ -7,10 +7,9 @@ app = Flask(__name__)
 def prime_number(number: int | str = 1) -> dict:
     number = int(number)
 
-    # src: stackoverflow
     is_prime = number > 1 and all(number % i for i in range(2, int(number ** 0.5) + 1))
 
-    return {"number": number, "isPrime": is_prime}
+    return jsonify({"number": number, "isPrime": is_prime})
 
 # 404 handler
 @app.errorhandler(404)
@@ -20,6 +19,6 @@ def page_not_found(status_code):
         "message": "Invalid endpoint"
     }
     
-    return Response(response=json.dumps(response), status=404, mimetype="application/json")
+    return jsonify(response), 404
 
 app.run(use_reloader=True, host="localhost", port=3000)
--- Modules/mod13/2.py
+++ Modules/mod13/2.py
@@ -1,29 +1,28 @@
 from flask import Flask, request, Response
 import mysql.connector
-import json
 
 app = Flask(__name__)
 
 @app.route('/airports/<icao>')
-def get_airport_by_icao(icao: str = ...):
-    db = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "password",
-        database = "airports", # created in a previous module
-        collation = "utf8mb4_unicode_ci"
-    )
-    
-    cursor = db.cursor(dictionary=True)
-    sql_query = f"SELECT ident, name, municipality FROM airport WHERE ident = '{icao.upper()}'"
-    
-    try:
-        cursor.execute(sql_query)
+def get_airport_by_icao(icao: str):
+    try: 
+        db = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "password",
+            database = "flight_game",
+            collation = "utf8mb4_unicode_ci"
+        )
+        
+        cursor = db.cursor(dictionary=True)
+        sql_query = "SELECT ident, name, municipality FROM airport WHERE ident=%s"
+        
+        cursor.execute(sql_query, (icao,))
         data = cursor.fetchone()
         
-        return data
+        return data if data else {"message": "No data found"}
     except Exception as error:
-        return {"error": error}
+        return error
     
 # 404 handler
 @app.errorhandler(404)
@@ -33,6 +32,6 @@ def page_not_found(status_code):
         "message": "Invalid endpoint"
     }
     
-    return Response(response=json.dumps(response), status=404, mimetype="application/json")
+    return response
 
 app.run(use_reloader=True, host="localhost", port=3000)
